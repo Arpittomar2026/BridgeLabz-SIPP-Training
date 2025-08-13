7440bb9fb80cf689bbc4debe975b0f308602f65b
Arpit tomar <arpittomar8055@gmail.com>
2025-08-13 11:14:31 +0530

    Voting age

--- src/Day2Programming/VotingCheck.java
+++ src/Day2Programming/VotingCheck.java
@@ -5,16 +5,15 @@ public class VotingCheck {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // Define array of 10 integers
         int[] ages = new int[10];
 
-        // Take user input for each student's age
+      
         for (int i = 0; i < ages.length; i++) {
             System.out.print("Enter age of student " + (i + 1) + ": ");
             ages[i] = sc.nextInt();
         }
 
-        // Loop through array using length property
+       
         for (int i = 0; i < ages.length; i++) {
             if (ages[i] < 0) {
                 System.out.println("Invalid age");
